f394110e95c016195647e00363c2084c93163a09
benwalcutt <bewalcut@email.uark.edu>
2015-02-16 15:03:50 -0600

    Finished
    Auto complete application finished

--- myautocomplete.java
+++ myautocomplete.java
@@ -49,65 +49,44 @@ class myautocomplete extends JTextField implements KeyListener {
 		Character input_char;
 		int divide;
 		
-		System.out.print("selection start TOP: ");
-				System.out.println(this.main_field.getSelectionStart());
-				System.out.print("selection end TOP: ");
-				System.out.println(this.main_field.getSelectionEnd());
-				System.out.println("----------");
-				System.out.println(e);
-				System.out.println("----------");
-		if (e.getKeyChar() != 8) {
+		input_char = null;
 		
-		//this.test_key = this.main_field.getText();
 		input_char = e.getKeyChar();
+		
+		if (input_char == 8) {
+			this.main_field.replaceSelection("");
+			this.test_key = this.main_field.getText();
+			e.consume();
+		}
+		else if (input_char == 10 || input_char == 12) {
+			this.test_key = this.main_field.getText();
+			this.main_field.setCaretPosition(test_key.length());
+			e.consume();
+		}
+		else {
 		this.main_field.replaceSelection(input_char.toString());
 		test_key = test_key.concat(input_char.toString());
-		
+
 		if (this.main_field.isFocusOwner() == true) {
 			return_key = tm.ceilingKey(test_key);
 			if (return_key.contains(test_key)) {
 				divide = test_key.length();
-				System.out.print("return key: ");
-				System.out.println(return_key);
-				System.out.print("test key: ");
-				
-				System.out.println(test_key);
-				System.out.print("divide: ");
-				System.out.println(divide);
 				
 				return_key = return_key.substring(divide);
 				i = test_key.substring(0);
 				i = i.concat(return_key);
 				this.main_field.setText(i);
-				System.out.print("i: ");
-				System.out.println(i);
+
 				this.main_field.setCaretPosition(divide);
 				this.main_field.select(divide, i.length());
-				System.out.print("selection start: ");
-				System.out.println(this.main_field.getSelectionStart());
-				System.out.print("selection end: ");
-				System.out.println(this.main_field.getSelectionEnd());
 				
 			}
 			else {
-				System.out.println("");
 			}
 		}
 			e.consume();
 		
 		}
-		else {
-			if (this.main_field.getSelectionEnd() == this.main_field.getSelectionStart()) {
-				System.out.print("selection start BS: ");
-				System.out.println(this.main_field.getSelectionStart());
-				System.out.print("selection end BS: ");
-				System.out.println(this.main_field.getSelectionEnd());
-				test_key = test_key.substring(0, test_key.length() - 1);
-				System.out.print("test_key BS: ");
-				System.out.println(test_key);
-			}
-		}
-		input_char = null;
+	}
 	}
 
-}
